460a2cdadc297ff52f88f1c7ebc26d8717adeae3
Yifei Zhou <yifei@xsjrdevl100.(none)>
2018-04-18 16:35:42 -0700

    fix indentation

--- src/python/test.py
+++ src/python/test.py
@@ -9,11 +9,13 @@ import time
 class Test:
   def cmp(self,A, B):
       if np.array_equal(A, B):
-	  print ("Success!\n")
+          print ("Success!\n")
       else:
-	  print ("not equal :(")
-	  sys.exit()  
-	  
+          print ("not equal :(")
+          sys.exit()  
+          
+        
+
   def multiply_and_cmp(self,C, A, B, X, m, n, post_scale):
       # Calculate golden C
       m64 = np.matmul(np.int64(A), np.int64(B))  # intermediate accumulation to 64 bits
@@ -23,26 +25,27 @@ class Test:
       o64m = o64d / (2 ** post_scale[1])
       C_cpu = np.int16(o64m)  # scale down for 16 bits
       if np.array_equal(C, C_cpu):
-	  print ("Success!\n")
+          print ("Success!\n")
       else:
-	  print ("Not equal!")
-	  print (C.shape, C_cpu.shape)
-	  np.savetxt("cpu_out.np", C_cpu, fmt="%d")
-	  np.savetxt("fpga_out.np", C, fmt="%d")
-	  np.savetxt("bias.np", X, fmt="%d")
-	  np.savetxt("A.np", A, fmt="%d")
-	  np.savetxt("B.np", B, fmt="%d")
-	  sys.exit();    
-	  
+          print ("Not equal!")
+          print (C.shape, C_cpu.shape)
+          np.savetxt("cpu_out.np", C_cpu, fmt="%d")
+          np.savetxt("fpga_out.np", C, fmt="%d")
+          np.savetxt("bias.np", X, fmt="%d")
+          np.savetxt("A.np", A, fmt="%d")
+          np.savetxt("B.np", B, fmt="%d")
+          sys.exit();    
+        
+        
   def test_basic_randint (self,A_range, B_range, bias_range, m, k, n, post_scale):
       mat_A = np.random.randint(low=-A_range, high=A_range, size=(m, k), dtype=np.int16)
 
       mat_B = np.random.randint(low=-B_range, high=B_range, size=(k, n), dtype=np.int16)  
       bias = []
       if bias_range != 0:
-	  bias = np.random.randint(low=-bias_range, high=bias_range, size=(m, n), dtype=np.int32)
+          bias = np.random.randint(low=-bias_range, high=bias_range, size=(m, n), dtype=np.int32)
       else:
-	  bias = np.zeros ((m, n), dtype=np.int32, order='C');      
+          bias = np.zeros ((m, n), dtype=np.int32, order='C');      
       self.test_basic(mat_A, mat_B, bias, post_scale)
 
   def test_basic_randint_shift (self,A_range, A_shift, B_range, B_shift, bias_range, bias_shift, m, k, n, post_scale):
@@ -52,23 +55,23 @@ class Test:
       mat_B = mat_B + B_shift   
       bias = []
       if bias_range != 0:
-	  bias = np.random.randint(low=-bias_range, high=bias_range, size=(m, n), dtype=np.int32)
+          bias = np.random.randint(low=-bias_range, high=bias_range, size=(m, n), dtype=np.int32)
       else:
-	  bias = np.zeros ((m, n), dtype=np.int32);    bias = bias + bias_shift
+          bias = np.zeros ((m, n), dtype=np.int32);    bias = bias + bias_shift
       self.test_basic(mat_A, mat_B, bias, post_scale)  
       
   def test_rand_basic (self,int_range, bias_range, num_iter, post_scale):  
       min_sz_exp = 8 
       for i in range(num_iter):
-	  print ("test_rand_basic iter: %d" % i)
-	  rand_m = random.randint(0, 5) 
-	  rand_k = random.randint(0, 5) 
-	  rand_n = random.randint(0, 5)       
-	  rand_m = 2 ** (rand_m + min_sz_exp) 
-	  rand_k = 2 ** (rand_k + min_sz_exp)
-	  rand_n = 2 ** (rand_n + min_sz_exp)
-	  self.test_basic_randint(int_range, int_range, bias_range, rand_m, rand_k, rand_n, post_scale)
-	  
+          print ("test_rand_basic iter: %d" % i)
+          rand_m = random.randint(0, 5) 
+          rand_k = random.randint(0, 5) 
+          rand_n = random.randint(0, 5)       
+          rand_m = 2 ** (rand_m + min_sz_exp) 
+          rand_k = 2 ** (rand_k + min_sz_exp)
+          rand_n = 2 ** (rand_n + min_sz_exp)
+          self.test_basic_randint(int_range, int_range, bias_range, rand_m, rand_k, rand_n, post_scale)
+          
   def test_basic(self,mat_A, mat_B, bias, post_scale = [1,1]):
       m = mat_A.shape[0]
       k = mat_A.shape[1]
@@ -86,10 +89,10 @@ class Test:
       gemx.execute()
       gemx.getMat(C_fpga)  
       if m > 4096 and n > 4096 and k > 4096:
-	print("Skip golden comparision because large matrix size")
-      else:
-	self.multiply_and_cmp(C_fpga, mat_A, mat_B, bias, m, n, post_scale)
-	  
+        print("Skip golden comparision because large matrix size")
+        else:
+        self.multiply_and_cmp(C_fpga, mat_A, mat_B, bias, m, n, post_scale)
+        
 class FcnTest(Test):       
   def test_basic(self,mat_A, mat_B, bias, post_scale=[1, 1]):
       m = mat_A.shape[0]
@@ -108,9 +111,9 @@ class FcnTest(Test):
       gemx.execute()
       gemx.getMat(C_fpga)  
       if m > 4096 and n > 4096 and k > 4096:
-	print("Skip golden comparision because large matrix size")
+        print("Skip golden comparision because large matrix size")
       else:
-	self.multiply_and_cmp(C_fpga, mat_A, mat_B, bias, m, n, post_scale)
-	
+        self.multiply_and_cmp(C_fpga, mat_A, mat_B, bias, m, n, post_scale)
+        
 class GemmTest(Test):               
   pass
