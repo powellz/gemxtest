5644beda083e5146a27281f6eafd4524b51fdca1
Bill Teng <xteng@xilinx.com>
2018-04-25 17:55:49 -0700

    add dockerfiles

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#!/bin/bash
+
+uid=`id -u $USER`
+gid=`id -g $USER`
+
+sudo docker build -t xlnx_cred --build-arg user=$USER --build-arg uid=$uid --build-arg gid=$gid -f Dockerfile.xlnx_cred . &&
+sudo docker build -t $USER/keras -f Dockerfile .
