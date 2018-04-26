66a6148e9e09e3b88fb1292cedd5de8b790fa390
Bill Teng <xteng@xilinx.com>
2018-04-26 15:46:40 -0700

    update fcn vcu1525 files and fcn kcu1500 config file

--- xclbins/fcn_vcu1525/xbinst/setup.sh
+++
@@ -1,6 +0,0 @@
-export XILINX_OPENCL=/home/xteng/work/HEAD_WORK/OPENCL_APPS_DEV/src/git/gemx/gemx/out_hw/xbinst
-export LD_LIBRARY_PATH=$XILINX_OPENCL/runtime/lib/x86_64:$LD_LIBRARY_PATH
-export PATH=$XILINX_OPENCL/runtime/bin:$PATH
-unset XILINX_SDACCEL
-unset XILINX_SDX
-unset XCL_EMULATION_MODE
