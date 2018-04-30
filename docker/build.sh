8ed86a837e10caed1a86a9202495c72daac59fbf
Bill Teng <xteng@xilinx.com>
2018-04-30 16:14:08 -0700

    support internal (within Xilinx) and external docker image build with same file

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,4 @@
 uid=`id -u $USER`
 gid=`id -g $USER`
 
-sudo docker build -t xlnx_cred --build-arg user=$USER --build-arg uid=$uid --build-arg gid=$gid -f Dockerfile.xlnx_cred . &&
-sudo docker build -t $USER/keras -f Dockerfile .
+sudo docker build -t $USER/keras --build-arg BASE_IMG=ubuntu:16.04 -f Dockerfile .
