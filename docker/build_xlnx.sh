f3ccb759633f92b6f8f878197aad2deccefa4d94
Bill Teng <xteng@xilinx.com>
2018-05-01 19:52:49 -0700

    fix build script

--- docker/build_xlnx.sh
+++ docker/build_xlnx.sh
@@ -3,5 +3,5 @@
 uid=`id -u $USER`
 gid=`id -g $USER`
 
-sudo docker build -t xlnx_cred --build-arg user=$uid --build-arg uid=$uid --build-arg gid=$gid -f Dockerfile.xlnx_cred . &&
+sudo docker build -t xlnx_cred --build-arg user=$USER --build-arg uid=$uid --build-arg gid=$gid --build-arg http_proxy=http://proxy:8080 --build-arg https_proxy=http://proxy:8080 -f Dockerfile.cred . &&
 sudo docker build -t $uid/keras --build-arg BASE_IMG=xlnx_cred -f Dockerfile .
