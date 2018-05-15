4c09f73e63f90fa137e9220f36f4a4835251e581
Yifei Zhou <y.zhou@xilinx.com>
2018-05-15 13:22:19 -0700

    update test scripts for spmv

--- regressions/gemx_Perf_run1Spmv_vu9p_fp32/run_board_custom.sh
+++ regressions/gemx_Perf_run1Spmv_vu9p_fp32/run_board_custom.sh
@@ -7,15 +7,12 @@ logs=()
 echo
 date
 
-nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/diag16k.mtx |& tee log-diag16k.txt
-nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/raefsky3.mtx |& tee log-raefsky3.txt
-nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/stomach.mtx |& tee log-stomach.txt
-nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/torso3.mtx |& tee log-torso3.txt
-#nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/Rucci1.mtx |& tee log-Rucci1.txt
+nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/diag16k.mtx.gz |& tee log-diag16k.txt
+nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/image_interp.mtx |& tee log-image_interp.txt
+nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 0 0 0 data/spmv/mario001.mtx |& tee log-mario001.txt
 nice out_host/gemx_api_spmv.exe out_hw/gemx.xclbin 65472 65472 500000 |& tee log-1.txt
 
-#logs="$logs log-diag16k.txt log-raefsky3.txt log-stomach.txt log-torso3.txt log-Rucci1.txt log-1.txt"
-logs="$logs log-diag16k.txt log-raefsky3.txt log-stomach.txt log-torso3.txt log-1.txt"
+logs="$logs log-diag16k.txt log-image_interp.txt log-mario001.txt log-1.txt"
  
 /bin/rm -f perf_gemm_api.csv
 egrep -h ^DATA_CSV $logs | grep DdrWidth | head -1 > perf_gemm_api.csv
