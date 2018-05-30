bb02f5d890c69081d9d78aebd3887c6639938f0e
Bill Teng <xteng@xilinx.com>
2018-05-30 16:27:09 -0700

    remove need to set LD_LIBRARY_PATH

--- rt_setup.sh
+++ rt_setup.sh
@@ -2,8 +2,6 @@
 
 export PYTHONPATH=./src/python
 export XILINX_OPENCL=$1/xbinst
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XILINX_OPENCL/runtime/lib/x86_64
-export PATH=$XILINX_OPENCL/runtime/bin:$PATH
 unset XILINX_SDACCEL
 unset XILINX_SDX
 unset XCL_EMULATION_MODE
